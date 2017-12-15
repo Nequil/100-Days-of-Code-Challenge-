ca6d297c4ae2138e9e602487b4ba91d45f1f6235
Nequil <Chaz1997@gmail.com>
2017-12-15 07:19:25 -0500

    updated string stuff
    more string information in python

---
+++ Project1/Gretting.py
@@ -0,0 +1,22 @@
+_author_='Dev'
+# # greeting = "Hello"
+# # name = input ("please enter your name ")
+# # print(greeting + ' '+ name)
+#
+# splitString = "This string has been\nsplit over\nseveral\nlines"
+# print(splitString)
+#
+# tabbedString = "1\t2\t3\t3\t4\t5"
+# print(tabbedString)
+
+print('The pet shop owner said "No, no \'e\'s uh,... he\s resting"')
+print ("the pet shop owner said \"No, no, 'e's uh, ...he's resting\"")
+
+
+
+anotherSplitString = """this string has been
+split
+several lines"""
+print(anotherSplitString)
+
+print('''the pet shop owner said "No, no 'e's uh ,...resting"''')
