e0ff691416837d7596a7abaa090885b5e6dbdb44
Nequil <Chaz1997@gmail.com>
2017-12-25 20:35:10 -0500

    if and strings

---
+++ Project1/ifprogram.py
@@ -0,0 +1,4 @@
+__author__ = 'dev'
+name = input("Please enter your name using the keyboard: ")
+age = input("How old are you, {0}? " .format(name))
+print(age)
---
+++ Project1/stingsandnumber.py
@@ -0,0 +1,12 @@
+__author__ = 'dev'
+gretting = "Bruce"
+_myName = "Charles"
+Charles45 = "Good"
+Charles_was_20 = "Hello"
+Greeting = "There"
+print(Charles_was_20 + ' ' + greeting)
+
+age = 24
+print(age)
+
+print(gretting + age)
