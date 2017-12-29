0dd81d919d5380f801992a389e6317089980ebd9
Nequil <Chaz1997@gmail.com>
2017-12-29 05:53:06 -0500

    ifproject

---
+++ Project1/Ifproject.py
@@ -0,0 +1,8 @@
+name = input("Please type your name.")
+age = int(input(Please type your age))
+
+if 18 <= age < 31:
+    print("Welcome to club 18-30 holidays, {0}".format(name))
+else:
+    print(
+        "Sorry but you are not in the age bracket for club 18-30 holidays, Thank you, {0}".format(name))
